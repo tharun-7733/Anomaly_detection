99bbf8c1576f5a9355f8732bb49ef2d85b37a26f
tharun-7733 <tharuntej7373@gmail.com>
2026-09-24 18:36:45 +0530

    Fix debian package clash

--- setup-ec2.sh
+++ setup-ec2.sh
@@ -9,7 +9,7 @@ sudo apt-get install -y python3 python3-pip libpcap-dev python3-venv
 
 # Navigate to backend directory and install Python dependencies globally for root
 cd backend
-sudo pip3 install -r requirements.txt --break-system-packages
+sudo pip3 install -r requirements.txt --break-system-packages --ignore-installed
 
 # Start the server with sudo (Requires sudo for raw socket packet sniffing)
 echo "Setup complete! To run the backend, execute:"
